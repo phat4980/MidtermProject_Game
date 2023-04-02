fccc39936325d33cb6020487b5c8e5249dd6f737
TPLouis <phat4980@gmail.com>
2023-04-03 02:20:12 +0700

    Few edit with AI Car

---
+++ Assets/Scripts/AI/CarWheel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf0859495a3b7b2e3695fbd52645b9d0850ea5b4a40a016f880e0b6834cf3673
+size 430
---
+++ Assets/Scripts/AI/Path.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cedd55414f5b939c5fbfb329ae5fee03959fe4e15849548d654775aeac953d63
+size 1096
