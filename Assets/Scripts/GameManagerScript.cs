7fde483a984ffba653a3aa86f3a3f6c32f10a6ce
TPLouis <phat4980@gmail.com>
2023-04-03 00:45:10 +0700

    Lap game

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4522deec70a2d737640ed5d4c9cd25fa7685fa9cf3de930ad175c230cf47cb0
+size 639
---
+++ Assets/Scripts/RaceScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:45fed7d78409905c0a7134d5ec3d88588822de5db0c4628075576ffdd597ffef
+size 3969
