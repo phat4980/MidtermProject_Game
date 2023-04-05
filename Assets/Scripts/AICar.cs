842b4a6d7e9982b3305a895e9f9f13d3f6cc5319
TPLouis <phat4980@gmail.com>
2023-04-06 02:54:41 +0700

    Update work

--- Assets/Scripts/AICar.cs
+++ Assets/Scripts/AICar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2f722adde94d2206cf28c62df1bfdc49fda39f1d867db4c77bc68852538b531
-size 2294
+oid sha256:fd3a4ae5d22ab70a39c592b511fe98f18ebd4c757fb16b56f815ea4b8f2885d6
+size 2320
